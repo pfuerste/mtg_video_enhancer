76497ecee4683cb2eea47aff21f0cb835a626db8
Philip Fuerste <philip.fuerste@gmail.com>
2020-11-17 15:55:14 +0100

    dleted supertype: much too narrow, small overlap

--- model/card.py
+++ model/card.py
@@ -1,12 +1,11 @@
 class Card:
     def __init__(
-        self, name, color, type, cmc, supertype, subtype, attack, toughness, loyality
+        self, name, color, type, cmc, subtype, attack, toughness, loyality
     ):
         self.name = name
         self.color = color
         self.type = type
         self.cmc = cmc
-        self.supertype = supertype
         self.subtype = subtype
         self.attack = attack
         self.toughness = toughness
