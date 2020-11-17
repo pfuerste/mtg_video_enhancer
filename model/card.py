b4648576f153cac6cbfb33a5613c4416db4067d7
Philip Fuerste <philip.fuerste@gmail.com>
2020-11-17 15:49:39 +0100

    added relevant card parameters

--- model/card.py
+++ model/card.py
@@ -1,3 +1,13 @@
 class Card:
-    def __init__(self, name):
+    def __init__(
+        self, name, color, type, cmc, supertype, subtype, attack, toughness, loyality
+    ):
         self.name = name
+        self.color = color
+        self.type = type
+        self.cmc = cmc
+        self.supertype = supertype
+        self.subtype = subtype
+        self.attack = attack
+        self.toughness = toughness
+        self.loyality = loyality
