1327d7f7a0f46d267f739c66a05477fe12e26d61
Philip Fuerste <philip.fuerste@gmail.com>
2020-11-17 16:56:24 +0100

    changed tests to incooperate new params

--- model/card_test.py
+++ model/card_test.py
@@ -1,6 +1,22 @@
 from card import Card
 
-def test_constructor_sets_name():
-    black_lotus = Card(name="Black Lotus")
-    assert black_lotus.name == "Black Lotus"
 
+def test_constructor_sets_name():
+    black_lotus = Card(
+        name="Black Lotus",
+        color="C",
+        type="artifact",
+        cmc=0,
+        subtype=None,
+        attack=None,
+        toughness=None,
+        loyality=None,
+    )
+    assert black_lotus.name == "Black Lotus"
+    assert black_lotus.color == "C"
+    assert black_lotus.type == "artifact"
+    assert black_lotus.cmc == 0
+    assert black_lotus.subtype == None
+    assert black_lotus.attack == None
+    assert black_lotus.toughness == None
+    assert black_lotus.loyality == None
