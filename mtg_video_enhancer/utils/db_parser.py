df5370feed729f53c8bee7ef98b67497247e5a9a
pfuerste <philip.fuerste@gmail.com>
2020-11-29 15:20:53 +0100

    parsing and downloading of db

--- mtg_video_enhancer/utils/db_parser.py
+++ mtg_video_enhancer/utils/db_parser.py
@@ -1,4 +1,7 @@
+import os
 import json
+from tqdm import tqdm
+import urllib.request
 
 
 def get_keys(json_path: str) -> list:
@@ -61,17 +64,66 @@ def get_faulty_cards(json_path: str) -> list:
     return faulty_cards
 
 
-def sort_by_keys():
-    pass
+# Return all card names which fulfill criteria
+def filter_by_format(json_path: str, keys: list) -> list:
+    file = open(json_path, "r")
+    j = json.load(file)
+    cards = [card for card in j]
+    filtered_cards = list()
+    for card in cards:
+        for key in keys:
+            if key in ['standard', 'future', 'historic', 'pioneer',
+                       'modern', 'legacy', 'pauper', 'vintage',
+                       'penny', 'commander', 'brawl', 'duel', 'oldschool']:
+                if card["legalities"][key] == "legal":
+                    # if card not in filtered_cards:
+                    filtered_cards.append(card)
+    return filtered_cards
 
 
-def download_card_imgs():
-    pass
+# Have a dict with only the desired keys
+def filter_keys(card_list: list, keys: list) -> list:
+    filtered_cards = list()
+    for card in card_list:
+        new_card = dict()
+        valid_vals = 0
+        for key in keys:
+            if type(key) is not str:
+                try:
+                    new_card[key[0]] = card[key[0]][key[1]]
+                    valid_vals += 1
+                except KeyError:
+                    continue
+            else:
+                try:
+                    new_card[key] = card[key]
+                    valid_vals += 1
+                except KeyError:
+                    continue
+        # nested continues...
+        if valid_vals == len(keys):
+            filtered_cards.append(new_card)
+    return filtered_cards
+
+
+def download_card_imgs(names_and_urls: list, img_dir: str):
+    print(f"Starting download of {len(names_and_urls)} images to {img_dir}.")
+    for card in tqdm(names_and_urls):
+        path = os.path.join(img_dir, card["name"])+".jpg"
+        url = card["image_uris"]
+        urllib.request.urlretrieve(url, path)
+        #print(f"to {path} from {url}")
+    print("Finished.")
 
 
 if __name__ == "__main__":
     path = "/mnt/c/Users/phili/_Documents/Projects/mtg_video_enhancer/oracle-cards-20201122100602.json"
-    keys = get_keys(path)
-    vals = get_values(path, ["artist"])
-    print(sorted(keys))
-    print(vals)
+    img_dir = "/mnt/c/Users/phili/_Documents/Projects/mtg_video_enhancer/card_imgs"
+    #keys = get_keys(path)
+    #vals = get_values(path, ["legalities"])
+    #print(sorted(keys))
+    #print(vals)
+    modern_cards = filter_by_format(path, ["modern"])
+    modern_names_urls = filter_keys(modern_cards, ["name", ["image_uris", "normal"]])
+    download_card_imgs(modern_names_urls, img_dir)
+    #print(len(modern_cards))
