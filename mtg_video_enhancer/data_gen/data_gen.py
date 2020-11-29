3600b4f7a1a6e0e2394dd046432237170d4716b3
pfuerste <philip.fuerste@gmail.com>
2020-11-29 17:23:44 +0100

    Filtering and error handling

--- mtg_video_enhancer/data_gen/data_gen.py
+++ mtg_video_enhancer/data_gen/data_gen.py
@@ -1,14 +1,18 @@
 import os
 import numpy as np
 import cv2 as cv
+from numpy.core.defchararray import endswith
 
 
 def get_random_imgs(img_dir: str, num: int) -> list:
-    files = os.listdir(img_dir)
-    files = np.random.choice(files, num, False)
+    files = [file for file in os.listdir(img_dir) if file.endswith(".jpg")]
+    try:
+        files = np.random.choice(files, num, False)
+    except ValueError:
+        pass
     paths = [os.path.join(img_dir, file) for file in files]
     return paths
 
 
 if __name__ == "__main__":
-    print(get_random_imgs("/mnt/c/Users/phili/Documents/card_imgs/historic", 5))
+    print(get_random_imgs("/mnt/c/Users/phili/Documents/card_imgs/historic", 999999999))
