fccce73711e7773dd1628d484871af1c924c6678
pfuerste <philip.fuerste@gmail.com>
2020-12-01 10:06:41 +0100

    added add_reflections

--- mtg_video_enhancer/data_gen/data_gen.py
+++ mtg_video_enhancer/data_gen/data_gen.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2 as cv
+
 # from numpy.core.defchararray import endswith
 
 
@@ -16,27 +17,75 @@ def get_random_imgs(img_dir: str, num: int) -> list:
 
 # def write_gt(, annot_csv: str):
 
-def create_img(bg_path: np.array, card_path: list,
-               annot_csv: str, out_dir: str):
+
+# TODO nicht nur auf ganzem Bild, auch kleinere
+def add_reflection(card_img: np.array, refl_img: np.array):
+    card_h, card_w = card_img.shape[:2]
+    min_refl_size = 50
+    upper_left = (
+        np.random.randint(0, card_h - min_refl_size),
+        np.random.randint(0, card_w - min_refl_size),
+    )
+    max_refl_size = np.subtract((card_h, card_w), upper_left)
+    # resize need (w, h)
+    refl_size = (
+        np.random.randint(min_refl_size - 1, max_refl_size[1]),
+        np.random.randint(min_refl_size - 1, max_refl_size[0]),
+    )
+    refl_img = cv.resize(src=refl_img, dsize=refl_size)
+
+    h, w = refl_img.shape[:2]
+    zeros = np.zeros(shape=card_img.shape, dtype=card_img.dtype)
+    a = cv.getGaussianKernel(w, 50)
+    b = cv.getGaussianKernel(h, 50)
+    c = b * a.T
+    d = c / c.max()
+    zeros[
+        upper_left[0] : upper_left[0] + h, upper_left[1] : upper_left[1] + w, 0
+    ] = (refl_img[:, :, 0] * d)
+    zeros[
+        upper_left[0] : upper_left[0] + h, upper_left[1] : upper_left[1] + w, 1
+    ] = (refl_img[:, :, 1] * d)
+    zeros[
+        upper_left[0] : upper_left[0] + h, upper_left[1] : upper_left[1] + w, 2
+    ] = (refl_img[:, :, 2] * d)
+   
+    t = cv.add(card_img, zeros)
+    cv.imwrite("vig2.jpg", t)
+    return t
+
+
+def create_img(bg_path: str, card_paths: list, annot_csv: str, out_dir: str):
     img = cv.imread(bg_path)
+    refl_img = cv.imread(
+        "/mnt/c/Users/phili/OneDrive/Bilder/Bewerbungsphoto.png"
+    )
     h, w = img.shape[:2]
 
-    for card in card_path:
+    for card in card_paths:
+        # TODO: weiße Ecken abschneiden
         card_img = cv.imread(card)
-        mask = 255*np.ones(card_img.shape, card_img.dtype)
-        
-        #h_blend = np.random.randint(0, h)
-        #w_blend = np.random.randint(0, w)
-        h_blend = int(card_img.shape[1]/2)
-        w_blend = int(card_img.shape[0]/2)
-        
-        out_img = cv.seamlessClone(card_img, img, mask,
-                              (h_blend, w_blend), cv.MONOCHROME_TRANSFER)
-    cv.imwrite(os.path.join(out_dir, "3.jpg"), out_img)
+        card_img = add_reflection(card_img, refl_img)
+
+        mask = 255 * np.ones(card_img.shape, card_img.dtype)
+
+        h_blend = int(card_img.shape[1] / 2)
+        w_blend = int(card_img.shape[0] / 2)
+
+        out_img = cv.seamlessClone(
+            card_img, img, mask, (h_blend, w_blend), cv.NORMAL_CLONE
+        )
+    cv.imwrite(os.path.join(out_dir, "refl.jpg"), out_img)
 
 
 if __name__ == "__main__":
-    bg_path = "/mnt/c/Users/phili/OneDrive/Bilder/photo_2020-07-06_09-37-19.jpg"
-    card_paths = get_random_imgs("/mnt/c/Users/phili/Documents/card_imgs/historic", 1)
-    out_dir = "/mnt/c/Users/phili/_Documents/Projects/mtg_video_enhancer/synth_data"
-    create_img(bg_path, card_paths, None, out_dir)
+    for i in range(100):
+        bg_path = (
+            "/mnt/c/Users/phili/OneDrive/Bilder/photo_2020-07-06_09-37-19.jpg"
+        )
+        card_paths = get_random_imgs(
+            "/mnt/c/Users/phili/Documents/card_imgs/historic", 1
+        )
+        out_dir = "/mnt/c/Users/phili/_Documents/Projects/mtg_video_enhancer/synth_data"
+        create_img(bg_path, card_paths, None, out_dir)
+    # add_reflection([cv.imread(bg_path), cv.imread(card_paths[1])])
